099f62f027673c93cf6ce980dbb48f6421ea2f7c
Timur Zheksimbaev <113367706+TimurZheksimbaev@users.noreply.github.com>
2025-01-25 18:08:03 +0300

    Added fullscreen and changed header color

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { useTonConnect } from './hooks/useTonConnect';
 import './App.css';
 import './style.css'
 import WebApp from "@twa-dev/sdk";
-import Header from "./components/Header";
+// import Header from "./components/Header";
 
 
 const App = () => {
@@ -35,10 +35,13 @@ const App = () => {
     WebApp.showPopup({title: "Settings", message: "You clicked settings button", buttons: [{type: "ok"}]})
   })
 
+  WebApp.setHeaderColor("#1C1D22")
+  WebApp.requestFullscreen()
+
 
   return (
     <div className="container">
-      <Header />
+      {/* <Header /> */}
       <div className="connect-button">
         <TonConnectButton />
       </div>
