1adb5c4a2ae162d28b50cd1baaa2ed643f6e0d11
Timur Zheksimbaev <113367706+TimurZheksimbaev@users.noreply.github.com>
2025-01-25 20:16:07 +0300

    as;ldfkj

--- src/App.tsx
+++ src/App.tsx
@@ -28,8 +28,8 @@ const App = () => {
     WebApp.SettingsButton.show()
   }
 
-  WebApp.setBackgroundColor(WebApp.backgroundColor as "#141319")
-  WebApp.setHeaderColor(WebApp.headerColor as "#1C1D22")
+  // WebApp.setBackgroundColor("#141319")
+  // WebApp.setHeaderColor(WebApp.backgroundColor as "bg_color")
 
   WebApp.SettingsButton.onClick(() => {
     WebApp.showPopup({title: "Settings", message: "You clicked settings button", buttons: [{type: "ok"}]})
